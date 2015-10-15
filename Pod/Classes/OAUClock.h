8651e67b659a75d0042a73d3b6583806240ecccc
Austin Ugbeme <hello@waado.co>
2015-10-14 23:41:48 -0700

    Initial commit

---
+++ Pod/Classes/OAUClock.h
@@ -0,0 +1,13 @@
+//
+//  OAUClock.h
+//  Pods
+//
+//  Created by Austin Ugbeme on 10/14/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OAUClock : UIView
+
+@end
---
+++ Pod/Classes/OAUClock.m
@@ -0,0 +1,21 @@
+//
+//  OAUClock.m
+//  Pods
+//
+//  Created by Austin Ugbeme on 10/14/15.
+//
+//
+
+#import "OAUClock.h"
+
+@implementation OAUClock
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
